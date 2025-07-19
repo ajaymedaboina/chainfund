7e12fe2077efa78ffbc0aa03ba65bcdc851f2954
AVADOOTHA RAJESH <148096139+avadootharajesh@users.noreply.github.com>
2025-07-20 00:14:13 +0530

    Update client.ts
    ClientId error

--- src/app/client.ts
+++ src/app/client.ts
@@ -2,9 +2,11 @@ import { createThirdwebClient } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
+let clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
 
 if (!clientId) {
+  // set to null
+  clientId = null;
   throw new Error("No client ID provided");
 }
 // Sample Hello msg
